91e7ae3ad486105ce850c6a187951e39592aeae1
Nizar Mahmoud <nizarmah@hotmail.com>
2020-06-16 22:46:51 +0300

    Updated Setup to Version 0.2 and Removed Python 2.7 Support

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="opencraft_courseware_search_filters",
-    version="0.1.0",
+    version="0.2.0",
     description="Custom search filters for Open edX LMS",
     author="OpenCraft",
     author_email="help@opencraft.com",
@@ -17,12 +17,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Framework :: Django",
-        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.2.13",
     ],
     packages=["opencraft_courseware_search_filters"],
 )
